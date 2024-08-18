df80cfd7944f6ee7bfa39e6163d89a25bf4db2fb
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-08-18 17:49:54 +0100

    broken section animations

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 // Elements
 const skillsList = document.querySelector(".skills-list");
+const overlay = document.querySelector('.modal-overlay');
+const nav = document.querySelector('.nav')
+const header = document.querySelector('.header');
 
 
 // Skills array (will eventually replace with fetch API)
@@ -37,3 +40,88 @@ function generateSkillImages() {
 }
 
 generateSkillImages();
+
+// Event listeners
+
+// Nav bar shortcuts - smooth scrolling
+
+document.querySelector('.nav-menu').addEventListener('click', function (e) {
+  e.preventDefault()
+  let targetElement = e.target;
+  console.log(e.target.parentElement)
+  //Matching strategy
+  if (e.target.classList.contains('nav-link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  } else {
+    const href = e.target.parentElement.getAttribute('href');
+    window.location.href = href;
+  }
+})
+
+// Nav bar - fade on hover
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav-link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav-link');
+    const logo = link.closest('.nav').querySelector('img');
+    const images = link.closest('.nav').querySelectorAll('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+      logo.style.opacity = this;
+    })
+
+    images.forEach(img => {
+      img.style.opacity = this;
+    });
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+nav.addEventListener('mouseout', handleHover.bind(1))
+
+// Reveal sections
+document.addEventListener('DOMContentLoaded', function () {
+  const allSections = document.querySelectorAll('.section')
+
+
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry)
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section-hidden')
+    observer.unobserve(entry.target);
+  }
+
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.5,
+  });
+
+
+  allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section-hidden');
+  })
+});
+// Project modal pop-ups
+
+document.querySelectorAll('.info-btn .btn').forEach(button => {
+  button.addEventListener('click', function () {
+    const projectBox = this.closest('.project-box');
+    const projectModal = projectBox.querySelector('.project-modal')
+    projectModal.classList.remove('hidden')
+    overlay.classList.remove('hidden');
+  });
+});
+
+document.querySelectorAll('.close-btn').forEach(button => {
+  button.addEventListener('click', function () {
+    const projectBox = this.closest('.project-box');
+    const projectModal = projectBox.querySelector('.project-modal')
+    projectModal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+});
